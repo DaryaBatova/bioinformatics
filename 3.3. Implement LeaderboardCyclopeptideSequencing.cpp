12a73335415db2db87ff5e6540d42e049fb58e64
DaryaBatova <darmorgan99@gmail.com>
2018-12-26 01:01:22 +0300

    Add task

--- 3.3. Implement LeaderboardCyclopeptideSequencing.cpp
+++ 3.3. Implement LeaderboardCyclopeptideSequencing.cpp
@@ -1,156 +1,146 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
 
-namespace _3._3__Leaderboard_Cyclopeptide_Sequencing
+using namespace std;
+
+static map<char, int> PeptideMap = { {'G', 57}, {'A', 71}, {'S', 87}, {'P', 97}, {'V', 99}, {'T', 101}, {'C', 103}, {'I', 113}, {'L', 113}, {'N', 114}, {'D', 115}, {'K', 128}, {'Q', 128}, {'E', 129}, {'M', 131}, {'H', 137}, {'F', 147}, {'R', 156},  {'Y', 163}, {'W', 186} };
+
+int MassOfPeptide(const string& Peptide)
 {
-    class Program
+    int mass = 0;
+    for (auto it : Peptide)
     {
-        static Dictionary<char, int> AminoacidMass = new Dictionary<char, int>()
-        {{'G', 57}, {'A', 71}, {'S', 87}, {'P', 97}, {'V', 99}, {'T', 101}, {'C', 103}, {'I', 113}, {'L', 113}, {'N', 114}, {'D', 115}, {'K', 128},
-         { 'Q', 128}, {'E', 129}, {'M', 131}, {'H', 137}, {'F', 147}, {'R', 156}, {'Y', 163}, {'W', 186}};
+        mass += PeptideMap[it];
+    }
+    return mass;
+}
 
-        static int Mass(string peptide)
+multiset<int> LinearSpectrum(const string& Peptide)
+{
+    multiset<int> Result;
+    Result.insert(0);
+    const char * pszSubArray;
+    int nMass{ 0 };
+    for (size_t i = 1; i < Peptide.size(); ++i)
+    {
+        pszSubArray = Peptide.c_str();
+        for (size_t j = 0; j < Peptide.size() - i + 1; ++j)
         {
-            int mass = 0;
-            for (int i = 0; i < peptide.Length; i++)
-                mass += AminoacidMass[peptide[i]];
-            return mass;
-        }
-
-        static List<string> Expand(List<string> peptides)
-        {
-            List<string> newPeptides = new List<string>();
-
-            foreach (var peptide in peptides)
+            nMass = 0;
+            for (size_t k = 0; k < i; ++k)
             {
-                foreach (var key in AminoacidMass.Keys)
-                {
-                    newPeptides.Add(peptide + key);
-                }
+                nMass += PeptideMap[pszSubArray[k]];
             }
-            return newPeptides;
-        }
-
-        static string LinearSpectrum(string peptide)
-        {
-            List<int> Linearspectrum = new List<int>();
-            string subpeptide = " ";
-            int mass = 0;
-
-            if (peptide.Length == 1)
-                return AminoacidMass[peptide[0]].ToString();
-
-            Linearspectrum.Add(0);
-
-            for (int i = 1; i < peptide.Length; i++)
-            {
-                for (int j = 0; j < peptide.Length; j++)
-                {
-                    if ((i + j) <= peptide.Length)
-                    {
-                        subpeptide = peptide.Substring(j, i);
-                        Linearspectrum.Add(Mass(subpeptide));
-                    }
-                }
-            }
-
-            Linearspectrum.Add(Mass(peptide));
-
-            Linearspectrum.Sort();
-            return string.Join(" ", Linearspectrum);
-        }
-
-        static int Score(string peptide, string spectrum)
-        {
-            List<string> peptideMass = LinearSpectrum(peptide).Split(' ').ToList();
-            List<string> spectrumMass = spectrum.Split(' ').ToList();
-
-            int score = 0;
-
-            foreach (var mass in peptideMass)
-            {
-                if (spectrumMass.Contains(mass))
-                {
-                    spectrumMass.Remove(mass);
-                    score++;
-                }
-            }
-
-            return score;
-        }
-
-        static List<string> Trim(List<string> Leaderboard, string Spectrum, int N)
-        {
-            Leaderboard.Sort((a, b) => Score(b, Spectrum).CompareTo(Score(a, Spectrum)));
-            if (Leaderboard.Count > N)
-            {
-                int last = N;
-                for (int i = N; i < Leaderboard.Count; i++)
-                {
-                    if (Score(Leaderboard[N - 1], Spectrum) == Score(Leaderboard[i], Spectrum))
-                    {
-                        last = i;
-                    }
-                    else break;
-                }
-
-                Leaderboard = Leaderboard.Take(last + 1).ToList();
-            }
-
-            return Leaderboard;
-        }
-
-        static int ParentMass(string spectrum)
-        {
-            int parentMass = int.Parse(spectrum.Split(' ').Last());
-            return parentMass;
-        }
-
-        static string LeaderboardCyclopeptideSequencing(string Spectrum, int N)
-        {
-            List<string> Leaderboard = new List<string>() { "" };
-            string LeaderPeptide = "";
-            while(Leaderboard.Count() > 0)
-            {
-                Leaderboard = Expand(Leaderboard);
-
-                List<string> LeaderboardExemple = new List<string>(Leaderboard);
-                foreach (var Peptide in LeaderboardExemple)
-                {
-                    if(Mass(Peptide) == ParentMass(Spectrum))
-                    {
-                        if(Score(Peptide, Spectrum) > Score(LeaderPeptide, Spectrum))
-                        {
-                            LeaderPeptide = Peptide;
-                        }
-                    }
-                    else if(Mass(Peptide) > ParentMass(Spectrum))
-                    {
-                        Leaderboard.Remove(Peptide);
-                    }
-                }
-                Leaderboard = Trim(Leaderboard, Spectrum, N);
-            }
-            return LeaderPeptide;
-        }
-
-        static void Main(string[] args)
-        {
-            int N = int.Parse(Console.ReadLine());
-            string Spectrum = Console.ReadLine();
-
-            string LeaderPeptide = LeaderboardCyclopeptideSequencing(Spectrum, N);
-
-            List<string> Mass = new List<string>();
-            foreach (var Peptide in LeaderPeptide)
-            {
-                Mass.Add(AminoacidMass[Peptide].ToString());
-            }
-
-            Console.WriteLine(string.Join("-", Mass));
+            Result.insert(nMass);
+            pszSubArray++;
         }
     }
+    nMass = 0;
+    for (size_t i = 0; i < Peptide.size(); ++i)
+        nMass += PeptideMap[Peptide[i]];
+    Result.insert(nMass);
+    return Result;
+}
+
+set<string> Expand(const set<string>& LeaderBoard)
+{
+    set<string> NewLeaderBoard;
+    for (const auto& itLeadBoard : LeaderBoard)
+    {
+        for (const auto& itPeptideMap : PeptideMap)
+        {
+            NewLeaderBoard.insert(itLeadBoard + itPeptideMap.first);
+        }
+    }
+    return NewLeaderBoard;
+}
+
+int Score(const string& sPeptide, const multiset<int>& Spectrum)
+{
+    int nScore{ 0 };
+    multiset<int> LinSpectr = LinearSpectrum(sPeptide);
+    vector<int> Result(LinSpectr.size() + Spectrum.size(), 0);
+    auto it = set_intersection(LinSpectr.begin(), LinSpectr.end(), Spectrum.begin(), Spectrum.end(), Result.begin());
+    Result.resize(it - Result.begin());
+    return Result.size();
+}
+
+set<string> Trim(set<string>& LeaderBoard, const multiset<int>& Spectrum, int N)
+{
+    vector<pair<int, string>> LeaderBoardVector;
+    for (const auto& it : LeaderBoard)
+    {
+        LeaderBoardVector.emplace_back(Score(it, Spectrum), it);
+    }
+    sort(LeaderBoardVector.begin(), LeaderBoardVector.end(), [](std::pair<int, string> a, std::pair<int, string> b) -> decltype(a.first > b.first)
+    {
+        return a.first > b.first;
+    });
+    set<string> NewLeaderBoard;
+    size_t i = 0;
+    while (i < N || (i >= N && LeaderBoardVector[i].first == LeaderBoardVector[N].first))
+    {
+        NewLeaderBoard.insert(LeaderBoardVector[i++].second);
+    }
+    return NewLeaderBoard;
+}
+
+string LeaderboardCyclopeptideSequencing(const multiset<int>& Spectrum, int N)
+{
+    set<string> LeaderBoard;
+    string LeaderPeptide{ "" };
+    LeaderBoard.insert(LeaderPeptide);
+    while (!LeaderBoard.empty())
+    {
+        LeaderBoard = Expand(LeaderBoard);
+        set<string> LeaderBoardCopy = LeaderBoard;
+        for (const auto& it : LeaderBoardCopy)
+        {
+            auto itMax = --(Spectrum.end()); //элемент, обратный первому
+            if (MassOfPeptide(it) == *itMax)
+            {
+                if (Score(it, Spectrum) > Score(LeaderPeptide, Spectrum))
+                {
+                    LeaderPeptide = it;
+                }
+            }
+            else if (MassOfPeptide(it) > *itMax)
+                LeaderBoard.erase(it);
+        }
+        if (!LeaderBoard.empty())
+            LeaderBoard = Trim(LeaderBoard, Spectrum, N);
+    }
+    return LeaderPeptide;
+}
+
+void OutputResult(const string& LeaderPeptide)
+{
+    
+    for (auto itChar = LeaderPeptide.begin(); itChar != LeaderPeptide.end(); itChar++)
+    {
+        cout << PeptideMap[*itChar];
+        if (itChar != (LeaderPeptide.end() - 1))
+            cout << "-";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    
+    int N{ 0 };
+    cin >> N;
+    int tmp{ 0 };
+    multiset<int> Spectrum;
+    while (cin >> tmp)
+    {
+        Spectrum.insert(tmp);
+    }
+    string LeaderPeptide = LeaderboardCyclopeptideSequencing(Spectrum, N);
+    OutputResult(LeaderPeptide);
 }
